0ea406add12bbafcf30dfc438656f9d949357e90
Athyrson Machado Ribeiro <athyrsonmr@gmail.com>
2018-02-01 23:57:54 -0300

    Alterações no layout

--- script.js
+++ script.js
@@ -43,24 +43,25 @@ f1.addEventListener("submit", writeBinN);
 function writeBinN(e){
 
     var base = parseInt(document.f1.base.value);
-    if (base > 64 || base <2){
-        alert("Valor Máximo = 64 e Valor minímo = 2");
-        document.f1.base.value = 64;
-        base = 64;
+    if (base > 32 || base <2){
+        alert("Valor Máximo = 32 e Valor minímo = 2");
+        document.f1.base.value = 32;
+        base = 32;
     } 
+    var b = base -1;
     var num = parseInt(document.f1.num.value);
-    if ((num >= Math.pow(2,31)) || (num < - Math.pow(2,31))){
+    if ((num >= Math.pow(2,b) || (num < - Math.pow(2,b)))){
         num = NaN;
-        alert("Número Inválido!")
+        alert("Número Inválido!");
     }
-    var binNum = to_binaryC2(num);
+    var binNum = to_binaryC2(num, base);
 
     var tr = document.createElement("tr");
     var tdN = document.createElement("td");
     var tdB = document.createElement("td");
 
     tdN.setAttribute("class", "tdN");
-    tdN.setAttribute("class", "tdB");
+    tdB.setAttribute("class", "tdB");
 
     var nodeN = document.createTextNode(num);
     var nodeB = document.createTextNode(binNum);    
@@ -70,33 +71,10 @@ function writeBinN(e){
     tr.appendChild(tdN);
     tr.appendChild(tdB);    
 
-    var table = document.getElementById("tab1");
+    var table = document.getElementById("tbody");
 
     table.appendChild(tr);
     
     e.preventDefault();
 };
 
-
-
-
-// Get the modal
-var modal = document.getElementById('myModal');
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-};
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-};
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-};
